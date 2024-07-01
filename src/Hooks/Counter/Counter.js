672a6ed620d1778a28cd4340c4362cb73135fa72
Hardy Lino <harv2222@gmail.com>
2024-07-01 15:14:25 +0200

    Ajout de useSwipeable pour la gestion de defilement des images dans le Composant Carousel

--- src/Hooks/Counter/Counter.js
+++ src/Hooks/Counter/Counter.js
@@ -1,4 +1,28 @@
-import { useMemo, useState } from 'react'
+//on va a defirnir le parametres a l'interieur de Counter
+//pour poivoir l'utilizer dans des autres composants
+import { useMemo, useState, useCallback } from 'react'
+
+function Counter(initialCount = 1, maxCount = 4) {
+  const [count, setCount] = useState(initialCount)
+
+  const increment = useCallback(() => {
+    setCount((prevCount) => (prevCount === maxCount ? 1 : prevCount + 1))
+  }, [maxCount])
+
+  const decrement = useCallback(() => {
+    setCount((prevCount) => (prevCount === 1 ? maxCount : prevCount - 1))
+  }, [maxCount])
+
+  const isVisible = useMemo(() => maxCount > 1, [maxCount])
+
+  return { count, setCount, increment, decrement, isVisible }
+}
+
+export default Counter
+
+/* Mon code de base pour bien travaille avec le composant Carousel
+
+import { useMemo, useState, useCallback } from 'react'
 //on va a defirnir le parametres a l'interieur de Counter
 //pour poivoir l'utilizer dans des autres composants
 function Counter(initialCount = 1, maxCount = 4) {
@@ -26,4 +50,4 @@ function Counter(initialCount = 1, maxCount = 4) {
   return { count, increment, decrement, isVisible }
 }
 
-export default Counter
+export default Counter*/
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,4 +1,133 @@
-import { useState } from 'react'
+import { useState, useEffect, useCallback } from 'react'
+import { useSwipeable } from 'react-swipeable'
+import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons'
+import Counter from '../../Hooks/Counter/Counter'
+import styles from '../Carousel/carousel.module.scss'
+
+function Carousel({ pictures }) {
+  const [currentIndex, setCurrentIndex] = useState(pictures.length)
+  const [offsetX, setOffsetX] = useState(0)
+  const [transitionEnabled, setTransitionEnabled] = useState(true)
+  const { count, setCount, increment, decrement, isVisible } = Counter(
+    1,
+    pictures.length,
+  )
+
+  const totalPictures = [...pictures, ...pictures, ...pictures]
+
+  const moveToNearestSlide = useCallback(() => {
+    const totalSlides = pictures.length
+    if (currentIndex < totalSlides) {
+      setTransitionEnabled(false)
+      setCurrentIndex(currentIndex + totalSlides)
+    } else if (currentIndex >= totalSlides * 2) {
+      setTransitionEnabled(false)
+      setCurrentIndex(currentIndex - totalSlides)
+    }
+  }, [currentIndex, pictures.length])
+
+  useEffect(() => {
+    if (!transitionEnabled) {
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          setTransitionEnabled(true)
+        })
+      })
+    }
+  }, [transitionEnabled])
+
+  useEffect(() => {
+    setCount((currentIndex % pictures.length) + 1)
+  }, [currentIndex, setCount, pictures.length])
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => prevIndex - 1)
+    decrement()
+  }
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => prevIndex + 1)
+    increment()
+  }
+
+  const handlers = useSwipeable({
+    onSwiping: (eventData) => {
+      if (Math.abs(eventData.deltaX) > 10) {
+        // Threshold to avoid small vibrations
+        setOffsetX(eventData.deltaX)
+      }
+    },
+    onSwipedLeft: () => {
+      setOffsetX(0)
+      nextSlide()
+    },
+    onSwipedRight: () => {
+      setOffsetX(0)
+      prevSlide()
+    },
+    onSwiped: () => {
+      setOffsetX(0)
+    },
+    //trackMouse: true,
+    preventDefaultTouchmoveEvent: true,
+  })
+
+  return (
+    <div className={styles.carousel__container}>
+      <div {...handlers} className={styles.pictures__container}>
+        <div
+          className={styles.carousel__track}
+          style={{
+            transform: `translateX(calc(-${currentIndex * 100}% + ${offsetX}px))`,
+            transition: transitionEnabled ? 'transform 0.3s ease-out' : 'none',
+          }}
+          onTransitionEnd={moveToNearestSlide}
+        >
+          {totalPictures.map((picture, index) => (
+            <img
+              key={index}
+              src={picture}
+              alt={`Slide ${index}`}
+              className={styles.carousel__image}
+            />
+          ))}
+        </div>
+        <div className={styles.button__container}>
+          {isVisible && (
+            <button className={styles.left__arrow} onClick={prevSlide}>
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </button>
+          )}
+          {isVisible && (
+            <button className={styles.right__arrow} onClick={nextSlide}>
+              <FontAwesomeIcon icon={faChevronRight} />
+            </button>
+          )}
+          {isVisible && (
+            <span className={styles.counter}>
+              {count}/{pictures.length}
+            </span>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Carousel.propTypes = {
+  pictures: PropTypes.arrayOf(PropTypes.string).isRequired,
+}
+
+export default Carousel
+
+{
+  /* MON CODE DE BASE 
+  import { useState } from 'react'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -12,8 +141,9 @@ import styles from '../Carousel/carousel.module.scss'
 
 function Carousel({ pictures }) {
   const [currentIndex, setCurrentIndex] = useState(0)
+
   //on va definir la variable qui vont prendre en compte le composant
-  //Counter
+  //Counter ave ses props
   const { count, increment, decrement, isVisible } = Counter(1, pictures.length)
 
   const prevSlide = () => {
@@ -45,30 +175,32 @@ function Carousel({ pictures }) {
           />
         ))}
         <div className={styles['button__container']}>
-          {/*on va utiliser la meme logique pour faire disparaitre les icons
-            qu'on a travaille dans le composant Counter "isVisible" */}
-          {isVisible && (
-            <button className={styles['left__arrow']} onClick={prevSlide}>
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </button>
-          )}
-          {isVisible && (
-            <button className={styles['right__arrow']} onClick={nextSlide}>
-              <FontAwesomeIcon icon={faChevronRight} />
-            </button>
-          )}
-          {isVisible && (
-            <span className={styles['counter']}>
-              {count}/{pictures.length}
-            </span>
-          )}
+          on va utiliser la meme logique pour faire disparaitre les icons
+            qu'on a travaille dans le composant Counter "isVisible" 
+            {isVisible && (
+              <button className={styles['left__arrow']} onClick={prevSlide}>
+                <FontAwesomeIcon icon={faChevronLeft} />
+              </button>
+            )}
+            {isVisible && (
+              <button className={styles['right__arrow']} onClick={nextSlide}>
+                <FontAwesomeIcon icon={faChevronRight} />
+              </button>
+            )}
+            {isVisible && (
+              <span className={styles['counter']}>
+                {count}/{pictures.length}
+              </span>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+  
+  Carousel.propTypes = {
+    pictures: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }
+  export default Carousel
+  */
 }
-
-Carousel.propTypes = {
-  pictures: PropTypes.arrayOf(PropTypes.string).isRequired,
-}
-export default Carousel
